ac0304d7c427282bca4b709fce7a1c8c743f9555
Scott Rubin <apreche@frontrowcrew.com>
2021-10-23 17:33:35 -0400

    Homepage (#1)
    Added a placeholder homepage, just so we have something to release.

---
+++ homepage/tests.py
@@ -0,0 +1,12 @@
+from django.test import Client, TestCase
+from django.urls import reverse
+
+
+class HomepageTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_homepage_ok(self):
+        url = reverse("homepage")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
