eaeaee5794f3c1d9ce9827057093b48b285ca21f
Scott Rubin <apreche@frontrowcrew.com>
2021-11-28 17:20:35 -0500

    Started show model and homepage view updated dependabot and lint workflows updated python package dependencies passed linting

--- betafrontrowcrew/settings.py
+++ betafrontrowcrew/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
     "django_celery_beat",
     "django_celery_results",
     "django_extensions",
-    "homepage",
+
+    "shows",
 ]
 
 
---
+++ shows/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from . import models
+
+def homepage(request):
+    template_name = "shows/homepage.html"
+    shows = models.Show.objects.all()
+    context = {
+        "shows": shows
+    }
+    return render(request, template_name, context)
