3aa7b01d01d9e01646b39784283ed9a581b8a36d
narsi1ion <tfklettl@gmail.com>
2022-01-06 22:25:11 +0100

    Added a custom context processor so that the shows header link can be populated dynamically on every view

--- shows/views.py
+++ shows/views.py
@@ -8,7 +8,6 @@ def homepage(request):
     template_name = "shows/homepage.html"
     shows = models.Show.objects.all()
     context = {
-        "shows": shows
     }
     return shortcuts.render(request, template_name, context)
 
