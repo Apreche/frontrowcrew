9e4ca22f028ad9c5b164e6cbd24823486fbaf248
Scott Rubin <apreche@frontrowcrew.com>
2023-02-25 18:56:59 -0500

    Add SQLite FDW to automated deployment (#226)

--- etl/sql_steps/13_fdw_drop.sql
+++ etl/sql_steps/13_fdw_drop.sql
@@ -1,4 +1,5 @@
 -- Remove the SQLite foreign data wrapper
 DROP SCHEMA IF EXISTS frc_etl CASCADE;
+DROP SERVER IF EXISTS frc_forum_source;
 DROP SERVER IF EXISTS frc_etl_source;
 DROP EXTENSION IF EXISTS sqlite_fdw;
